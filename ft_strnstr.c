be7debcd4f6b9c42551ab720a117b252581bd81c
Soufyane Marsi <smarsi@e1r8p10.1337.ma>
2023-11-05 21:08:57 +0100

    strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 10:23:49 by smarsi            #+#    #+#             */
+/*   Updated: 2023/11/05 17:40:10 by smarsi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!*needle)
+		return ((char *) haystack);
+	i = -1;
+	while (haystack[++i] && i < len)
+	{
+		if (haystack[i] == needle[0])
+		{
+			j = 0;
+			while (haystack[i + j] && haystack[i + j] == needle[j])
+				j++;
+			if (!needle[j] && (i + j) <= len)
+				return ((char *) haystack + i);
+		}
+	}
+	return (NULL);
+}
