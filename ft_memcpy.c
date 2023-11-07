2f5c2402631046a6997336be46a9cd16442e30de
Soufyane Marsi <smarsi@e1r8p11.1337.ma>
2023-11-07 10:12:29 +0100

    memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 18:40:03 by smarsi            #+#    #+#             */
+/*   Updated: 2023/11/07 09:42:50 by smarsi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*dest;
+	unsigned char	*source;
+
+	if (dst == src)
+		return (dst);
+	dest = (unsigned char *)dst;
+	source = (unsigned char *)src;
+	while (n--)
+	{
+		*dest = *source;
+		dest++;
+		source++; 
+	} 
+	return (dst);
+}
