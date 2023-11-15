ca4e7dc4b878d1c9ae8bddcd759841232781a81b
Soufyane Marsi <smarsi@e1r4p3.1337.ma>
2023-11-15 11:01:01 +0100

    lstadd_back_bonus

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 15:05:11 by smarsi            #+#    #+#             */
+/*   Updated: 2023/11/14 21:50:00 by smarsi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*head;
+
+	if (!lst)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		head = ft_lstlast(*lst);
+		head->next = new;
+	}
+}
